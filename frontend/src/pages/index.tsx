c6481402a810e7851145327e30aba00e74cdf72d
lbholmes1998 <lbholmes1998@gmail.com>
2023-10-02 21:50:44 +0100

    Get and return status from server

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+
+type ServerStatus = {
+    status: string
+}
+
+export const getServerSideProps = (async (context) => {
+    const res = await fetch("http:/localhost:8000")
+    const serverStatus = await res.json()
+    return {props: {serverStatus} }
+}) satisfies GetServerSideProps<{serverStatus: ServerStatus}>
+
+export default function Status({
+    serverStatus,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    return serverStatus.status
+}
