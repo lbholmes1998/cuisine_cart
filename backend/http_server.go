5aa1730136b8360511f7da5c9ff7a3d1b0ea7ccd
lbholmes1998 <lbholmes1998@gmail.com>
2023-10-17 19:02:44 +0100

    checkpoint recipe info route

--- backend/http_server.go
+++ backend/http_server.go
@@ -18,6 +18,7 @@ const (
 	ADMIN_PASSWORD = "admin"
 )
 
+const spoonacularBaseURL = "https://api.spoonacular.com/recipes"
 const spoonacularRecipeURL = "https://api.spoonacular.com/recipes/complexSearch"
 const apiKey = "822b0382280a46eab7b8e285da8cbfee"
 
@@ -38,6 +39,57 @@ func GetRecipes(w http.ResponseWriter, r *http.Request) {
 
 	// Parse spoonacular URL
 	parsedURL, err := url.Parse(spoonacularRecipeURL)
+	if err != nil {
+		fmt.Println("Error parsing URL", err)
+		return
+	}
+
+	// Store query params
+	queryParams := make(map[string]string)
+
+	// Existing query params
+	currentQuery := r.URL.Query()
+
+	// Iterate through params and add to map
+	for k, v := range currentQuery {
+		if len(v) > 0 {
+			// Only add single val for each query param
+			queryParams[k] = v[0]
+		}
+	}
+
+	// Construct query string
+	var newQueryElements []string // Pretty sure this means string array
+	for k, v := range queryParams {
+		newQueryElements = append(newQueryElements, fmt.Sprintf("%s=%s", k, v))
+	}
+
+	newQueryString := strings.Join(newQueryElements, "&")
+
+	parsedURL.RawQuery = newQueryString
+
+	fmt.Println(parsedURL.String())
+
+	resp, err := client.R().
+		SetHeader("Accept", "application/json").
+		SetHeader("x-api-key", apiKey).
+		Get(parsedURL.String())
+
+	// Send response to frontend
+	w.Write(resp.Body())
+
+	if err != nil {
+		log.Fatal("Error sending request")
+	}
+}
+
+func RecipeInfo(w http.ResponseWriter, r *http.Request) {
+	client := resty.New()
+
+	// Allow CORS
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	parsedURL, err := url.Parse(spoonacularBaseURL)
 	if err != nil {
 		fmt.Println("Error parsing URL", err)
 		return
@@ -103,6 +155,8 @@ func main() {
 	router.HandleFunc("/status", statusHandler).Methods("GET")
 	// router.Path("/recipes/{queryPeramns}").HandlerFunc(GetRecipes)
 	router.HandleFunc("/api/recipes", GetRecipes)
+	router.HandleFunc("/api/recipes/info", RecipeInfo)
+
 	err := http.ListenAndServe(CONN_HOST+":"+CONN_PORT, router)
 	if err != nil {
 		log.Fatal("Error starting HTTP server : ", err)
