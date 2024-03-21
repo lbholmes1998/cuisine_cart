e6c262ce3865b91b4aac2da1de69181c26c54d70
4holml09 <4holml09@solent.ac.uk>
2024-03-21 18:58:27 +0000

    schemas and type inference for recipe search

---
+++ cuisine-cart/models/Recipes.ts
@@ -0,0 +1,29 @@
+// Todo - Write types, schemas, etc for fetched recipe data
+import { z } from 'zod'
+
+// TODO - Add support for pagination
+
+
+// Basic recipe schema
+const BasicRecipeSchema = z.object({
+    "number": z.number(),
+    "offset": z.number(),
+    "totalResults": z.number(),
+})
+
+// Basic recipe info schema
+const RecipeSchema = z.object({
+    "id": z.number(),
+    "title": z.string(),
+    "image": z.string(),
+    "imageType": z.string(),
+})
+
+export const RecipeSchemaWithPhotos = BasicRecipeSchema.extend({
+    results: z.array(RecipeSchema)
+})
+
+
+// infer ts types
+export type Recipe = z.infer<typeof RecipeSchema>
+export type RecipeResults = z.infer<typeof RecipeSchemaWithPhotos>
