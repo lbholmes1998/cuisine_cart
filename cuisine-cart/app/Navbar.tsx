605726afc19457aad9d9b0c61cb60b40af5da56f
lbholmes <lbholmes1998@gmail.com>
2024-03-21 12:33:00 +0000

    Move navbar back to own component

---
+++ cuisine-cart/app/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+
+    return (
+        <>
+            <div className="w-full h-20 bg-slate-200 sticky top-0 z-10">
+                <div className="container flex items-center px-4 h-full m-auto">
+                    <div className="flex justify-between items-center m-auto h-full">
+                        <ul className="hidden md:flex gap-x-6 text-black">
+                            <li>
+                                <Link href="/">
+                                    <p>Home</p>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/recipe/recipeSearch">
+                                    <p>Search Recipes</p>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/about">
+                                    <p>about</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+
+export default Navbar;
