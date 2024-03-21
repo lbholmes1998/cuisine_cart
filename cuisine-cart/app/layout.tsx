aea17a4a7c8a27366936128a45771ec3f2e7201e
lbholmes <lbholmes1998@gmail.com>
2024-03-21 12:33:09 +0000

    add navbar component

--- cuisine-cart/app/layout.tsx
+++ cuisine-cart/app/layout.tsx
@@ -1,39 +1,24 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import type { Metadata } from "next";
+import Navbar from "./Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = {
+    title: 'Cuisine Cart',
+    description: 'Find tasty recipes!'
+}
+
 
 export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
-            <div className="w-full h-20 bg-slate-200 sticky top-0 z-10">
-                <div className="container flex items-center px-4 h-full m-auto">
-                    <div className="flex justify-between items-center m-auto h-full">
-                        <ul className="hidden md:flex gap-x-6 text-black">
-                            <li>
-                                <Link href="/">
-                                    <p>Home</p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/recipe/recipeSearch">
-                                    <p>Search Recipes</p>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/about">
-                                    <p>about</p>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </nav>
-        {children}
+        <Navbar/>
+        <main className="max-width-6xl mx-auto px-6">
+            {children}
+        </main>
       </body>
     </html>
   );
