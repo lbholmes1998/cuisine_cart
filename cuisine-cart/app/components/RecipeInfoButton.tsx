cd979014db6b90d1334bbfdae8c58d061a777c8e
4holml09 <4holml09@solent.ac.uk>
2024-03-22 13:19:28 +0000

    Button comp to handle fetching recipe info

---
+++ cuisine-cart/app/components/RecipeInfoButton.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React, { useState, FormEvent } from 'react';
+import { useRouter } from 'next/navigation';
+
+type Props = {
+    recipeId? : number | undefined, // union type
+}
+
+export default function RecipeInfoButton({ recipeId }: Props) {
+
+    //const [recipeId, setRecipeId] = useState('');
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push(`/recipe/info/${recipeId}`)
+        //setRecipeId('')
+    }
+
+    return (
+
+        <button className='mx-auto justify-center rounded-md bg-white px-3 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50' onClick={handleClick}>More details</button>
+    );
+}
