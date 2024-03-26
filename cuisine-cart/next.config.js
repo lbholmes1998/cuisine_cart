53c355ea2b62e9b589a36de2a83ec4fb2f682fa4
4holml09 <4holml09@solent.ac.uk>
2024-03-26 17:42:54 +0000

    add url for recipe imgs

--- cuisine-cart/next.config.js
+++ cuisine-cart/next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'spoonacular.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.spoonacular.com'
+      }
     ],
   },
   reactStrictMode: true,
