49cddf097b0419b70470c78a1a72c0c795f2d06c
lbholmes <lbholmes1998@gmail.com>
2024-07-02 18:12:05 +0100

    bad approach to saving recipes to db

--- cuisine-cart/lib/db/saveRecipe.ts
+++ cuisine-cart/lib/db/saveRecipe.ts
@@ -1,17 +1,48 @@
 import clientPromise from "./mongodb";
 import { getSession } from '@auth0/nextjs-auth0';
+import FetchUserInfo from "./fetchUserInfo";
+import { RecipeInfoResults} from '@/models/RecipeInfo';  //types
 
 // Adds recipe name & URL to users database document
-export default async function SaveRecipe(recipeUrl: string) {
+
+interface SavedRecipe {
+    url: string | undefined,
+    name: string | undefined
+}
+
+// export default async function SaveRecipe(recipeInfo: SavedRecipe) {
+//     const session = await getSession()
+//     const user = await session?.user
+
+//     const client = await clientPromise
+//     const db = await client.db("cuisine_cart")
+
+//     try {
+//         // test if we can update array
+//         const update = await db.collection("users").updateOne({email: user?.email}, {$addToSet:{savedRecipes: {name: recipeInfo.name, url: recipeInfo.url}}})
+//     } catch(e) {
+//         console.log(e)
+//     }
+// }
+
+export default async function addRecipeToDb(recipe:SavedRecipe) {
     const session = await getSession()
     const user = await session?.user
-
-    const client = await clientPromise
-    const db = await client.db("cuisine_cart")
-
     try {
-        const updateSaved = {}
-    } catch(e) {
-        console.log(e)
+        if (recipe) {
+            const response = await fetch('/api/db/saveRecipe', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: user?.email,
+                    url: recipe.url,
+                    name: recipe.name
+                })
+            })
+        }
+    } catch (e) {
+        console.log("Error sending user info to server!", e)
     }
 }
