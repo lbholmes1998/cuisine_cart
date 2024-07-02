9074528d38cfa5fcea9f9fa93cc6b97cbf325217
lbholmes <lbholmes1998@gmail.com>
2024-07-02 16:41:44 +0100

    add url to model

--- cuisine-cart/models/RecipeInfo.ts
+++ cuisine-cart/models/RecipeInfo.ts
@@ -6,6 +6,7 @@ const BasicRecipeInfoSchema = z.object({
     "aggregateLikes": z.number(),
     "title": z.string(),
     "image": z.string(),
+    "url": z.string(),
     "servings": z.number(),
     "pricePerServing": z.number(),
     "summary": z.string(),
