1ada5a07912a393944e4efd4b0733f6bfd1f3e77
lbholmes <lbholmes1998@gmail.com>
2024-07-02 18:11:45 +0100

    Show saved recipes on user page

--- cuisine-cart/app/user/page.tsx
+++ cuisine-cart/app/user/page.tsx
@@ -4,16 +4,14 @@ import FetchUserInfo from '@/lib/db/fetchUserInfo';
 export default async function UserInfo() {
   const user = await FetchUserInfo()
 
-  console.log(user?.savedRecipes)
-
   return (
     user && (
       <div>
         <p>User ID: {user.userId}</p>
         <p>User Email: {user.email}</p>
         <p className='mt-3'>Saved Recipes</p>
-        {user.savedRecipes.length >=1 && user.savedRecipes.map((recipe: string) => (
-          <p key={recipe}>{recipe}</p>
+        {user.savedRecipes.length >=1 && user.savedRecipes.map((recipe: any) => (
+          <p key={recipe.name}>{recipe.name}</p>
         ))}
       </div>
     )
