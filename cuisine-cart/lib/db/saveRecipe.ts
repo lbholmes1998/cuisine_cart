4fd476f61159b436487571b698ee8faa7e5305d5
lbholmes <lbholmes1998@gmail.com>
2024-07-02 16:42:19 +0100

    add saved recipes to db checkpoint

--- cuisine-cart/lib/db/saveRecipe.ts
+++ cuisine-cart/lib/db/saveRecipe.ts
@@ -1,6 +1,17 @@
 import clientPromise from "./mongodb";
+import { getSession } from '@auth0/nextjs-auth0';
 
-// Adds recipe URL to users database document
+// Adds recipe name & URL to users database document
 export default async function SaveRecipe(recipeUrl: string) {
+    const session = await getSession()
+    const user = await session?.user
 
+    const client = await clientPromise
+    const db = await client.db("cuisine_cart")
+
+    try {
+        const updateSaved = {}
+    } catch(e) {
+        console.log(e)
+    }
 }
